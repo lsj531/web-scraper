054b14ff2a42629a3cab3b2139eb5320b9239a8f
OP <lsj531@naver.com>
2021-04-19 23:26:14 +0900

    Indeed+Stackoverflow job scraping! +Web interface with Flask

--- indeed_scraper/indeed.py
+++ indeed_scraper/indeed.py
@@ -2,11 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-
-def get_last_page():
-    result = requests.get(URL)
+def get_last_page(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class": "pagination"})
 
@@ -39,11 +37,11 @@ def extract_job(html):
             'link': f"https://www.indeed.com/viewjob?jk={job_id}"}
 
 
-def extract_jobs(last_pages):
+def extract_jobs(last_pages, url):
     jobs = []
     for page in range(last_pages):
         print(f"Scraping Indeed: Page: {page}")
-        result = requests.get(f"{URL}&start={page * LIMIT}")
+        result = requests.get(f"{url}&start={page * LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
         for result in results:
@@ -52,7 +50,8 @@ def extract_jobs(last_pages):
     return jobs
 
 
-def get_jobs():
-    last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+def get_jobs(word):
+    url = f"https://www.indeed.com/jobs?q={word}&limit={LIMIT}"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
     return jobs
--- indeed_scraper/main.py
+++ indeed_scraper/main.py
@@ -1,8 +1,55 @@
+from flask import Flask, render_template, request, redirect, send_file
 from indeed import get_jobs as get_indeed_jobs
 from so import get_jobs as get_so_jobs
 from save import save_to_file
 
-indeed_jobs = get_indeed_jobs()
-so_jobs = get_so_jobs()
-jobs = so_jobs + indeed_jobs
-save_to_file(jobs)
+app = Flask("Job Scraper")
+
+db = {}
+
+
+@app.route("/")
+def home():
+    return render_template("interface.html")
+
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            indeed_jobs = get_indeed_jobs(word)
+            so_jobs = get_so_jobs(word)
+            jobs = so_jobs + indeed_jobs
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html",
+        searchingBy=word,
+        resultsNumber=len(jobs),
+        jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
+
+
+app.run(host="127.0.0.1")
